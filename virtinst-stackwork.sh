7c1b37ccddcd44d86a166d5b507fa8ce6b2a2153
Akihiro Motoki <motoki@da.jp.nec.com>
2014-11-23 01:33:13 +0900

    Update the spec of stackwork

--- virtinst-stackwork.sh
+++ virtinst-stackwork.sh
@@ -4,18 +4,18 @@ TOP_DIR=$(cd $(dirname "$0") && pwd)
 
 NAME=stackwork
 RELEASE_NAME=trusty
-NUM_CPU=4
-MEMORY=4096
+NUM_CPU=10
+MEMORY=24576
 ARCH=amd64
 VIRT_ARCH=x86_64
 
-#SITE=http://ftp.riken.go.jp/Linux/ubuntu
-#LOCATION=$SITE/dists/$RELEASE_NAME/main/installer-$ARCH/
-LOCATION=$HOME/iso/ubuntu/ubuntu-14.04.1-server-amd64.iso
+SITE=http://ftp.riken.go.jp/Linux/ubuntu
+LOCATION=$SITE/dists/$RELEASE_NAME/main/installer-$ARCH/
+#LOCATION=$HOME/iso/ubuntu/ubuntu-14.04.1-server-amd64.iso
 
 IMAGEDIR=/var/lib/libvirt/images
 DISK1FORMAT=raw
-DISK1SIZE=64
+DISK1SIZE=400
 DISK1FILE=$IMAGEDIR/$NAME.img
 
 sudo virt-install \
